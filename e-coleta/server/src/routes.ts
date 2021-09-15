87c7ac118895f9e06b42e2fec10c8571899c11ff
franciscodara <franciscodara@gmail.com>
2021-09-15 14:50:42 -0300

    Finalizaçao da api

--- e-coleta/server/src/routes.ts
+++ e-coleta/server/src/routes.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import ItemsController from './controllers/ItemsController';
+import PointsController from './controllers/PointsController';
 
 const routes = express.Router();
+const pointController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/', (request, response) => {
-    return response.json({message:'Hello World!'});
-});
+// List all registered items
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointController.create);
+routes.get('/points', pointController.index);
+routes.get('/points/:id', pointController.show);
 
 export default routes;
